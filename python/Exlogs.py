51c19b5a34ec7d59fab8ee2c005150f075ccb859
JuanPerezCrespo <juan.perez.crespo@alumnos.upm.es>
2025-10-30 12:52:22 +0100

    Requests

---
+++ python/Exlogs.py
@@ -0,0 +1,18 @@
+import logging
+import pandas as pd
+logging.basicConfig(level=logging.INFO)
+logging.info("Script started")
+logging.warning("Missing value encountered")
+
+# Error Handling with try/except:
+try:
+    price = float("20")
+except ValueError:
+    logging.error("Failed to convert price")
+
+# Combine both:
+try:
+    df = pd.read_csv("input.csv")
+except FileNotFoundError:
+    logging.error("File not found")
+    exit(1)
